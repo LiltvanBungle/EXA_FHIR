71e25afcbb790bfc947eb6751c3cc811c60a67ef
LiltvanBungle <glenbullivant84@gmail.com>
2022-02-19 22:24:42 +0000

    added mongodb and config file

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 import json
 
+"C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath d:\EXA_FHIR\EXA_FHIR\mongodb\data --auth
---
+++ myConfig.py
@@ -0,0 +1,4 @@
+
+# start server:
+#"C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath d:\EXA_FHIR\EXA_FHIR\mongodb\data --auth
+#
