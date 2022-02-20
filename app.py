75ed1841407deab9fac351f557ecce664a7a776f
LiltvanBungle <glenbullivant84@gmail.com>
2022-02-20 20:31:40 +0000

    tested to work, all files added to db

--- app.py
+++ app.py
@@ -3,21 +3,41 @@ import pymongo
 import myConfig
 from os import listdir
 
-#set connection string from config file
+#set db info from config file
 conn_str = myConfig.connection()
 
 # set a 5-second connection timeout
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
 
+# set db/collection
+db = client[myConfig.database()]
+col = db[myConfig.collection()]
 
-try:
-    print(client.server_info())
-except Exception:
-    print("Unable to connect to the server.")
+#test connection
+#try:
+#    print(client.server_info())
+#except Exception:
+#    print("Unable to connect to the server.")
 
+# make a list of files in the data folder
 filenames = listdir("data/")
-print(filenames)
+# print(filenames) 
 
-file = open("data/" + filenames[0])
-file = json.load(file)
+# loop over filenames
+i = 0
+while i < len(filenames):
+    file = open("data/" + filenames[i], "r", encoding='utf-8') #encoding for special characters
+    try:
+        file = json.load(file) #parse and validate json
+        print("validated " + filenames[i] + "\n")
+        x = col.insert_one(file)
+    except Exception:
+        print("Error loading file:" + filenames[i])
+
+    i += 1
+
+
+
+
+
 
--- myConfig.py
+++ myConfig.py
@@ -7,3 +7,11 @@
 def connection():
     return "mongodb://localhost:27017"
 
+# db name
+def database():
+    return "EXA"
+
+# collection name
+def collection():
+    return "FHIRdata"
+
