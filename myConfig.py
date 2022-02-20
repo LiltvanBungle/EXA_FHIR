202c8ea41e643dcdd67d3d1f5139d5a22cafa981
LiltvanBungle <glenbullivant84@gmail.com>
2022-02-20 23:40:22 +0000

    docker issues

--- myConfig.py
+++ myConfig.py
@@ -1,6 +1,6 @@
 
 # start server:
-#"C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath d:\EXA_FHIR\EXA_FHIR\mongodb\data --auth
+#"C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath d:\EXA_FHIR\EXA_FHIR\mongodb\data
 #mongo shell: "C:\Program Files\MongoDB\Server\5.0\bin\mongo.exe"
 
 #connection string
